ec33d22ce3d599bf81459192e12f80769d9e7e6b
NabilChabab <nabil.chababnabil@gmail.com>
2024-09-24 12:10:20 +0100

    Quick Fix in MaterialInterface

--- src/repository/interfaces/MaterialInterface.java
+++ src/repository/interfaces/MaterialInterface.java
@@ -3,7 +3,6 @@ package repository.interfaces;
 import domain.entities.Material;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface MaterialInterface  <T extends Material> extends CrudInterface<Material>{
     List<Material> findAllByProjectId(int id);
