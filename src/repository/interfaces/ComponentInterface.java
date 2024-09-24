1c662ce8068491ce43afff2f4a9193dd5357211e
NabilChabab <nabil.chababnabil@gmail.com>
2024-09-24 12:09:26 +0100

    Quick Fix in client menu

--- src/repository/interfaces/ComponentInterface.java
+++ src/repository/interfaces/ComponentInterface.java
@@ -2,9 +2,6 @@ package repository.interfaces;
 
 import domain.entities.Component;
 
-import java.util.List;
-import java.util.Optional;
-
 public interface ComponentInterface  <T extends Component> extends CrudInterface<Component>{
 
     double findTvaForComponent(int id);
