636208448ed443733eb231e04415776eebca0e90
NabilChabab <nabil.chababnabil@gmail.com>
2024-09-24 12:11:10 +0100

    WorkForceInterface.java

--- src/repository/interfaces/WorkForceInterface.java
+++ src/repository/interfaces/WorkForceInterface.java
@@ -3,7 +3,6 @@ package repository.interfaces;
 import domain.entities.WorkForce;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface WorkForceInterface<T extends WorkForce> extends CrudInterface<WorkForce> {
     List<WorkForce> findAllByProjectId(int id);
